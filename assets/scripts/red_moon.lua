12502f84e9396aed1d73353a81319a26f88cb4fb
kee-reel <c4rb0n_unit@protonmail.com>
2022-06-21 00:32:48 +0300

    introduce init Lua function

--- assets/scripts/red_moon.lua
+++ assets/scripts/red_moon.lua
@@ -1,5 +1,7 @@
 period = 0
-self.transform:set_scale(vec3:new(0.1, 0.1, 0.1))
+function init()
+    self.transform:set_scale(vec3:new(0.1, 0.1, 0.1))
+end
 function update()
     self.transform:set_pos(vec3:new(0, 3*math.sin(period/5), 1.5*math.cos(period/5)));
     period = period + 0.005

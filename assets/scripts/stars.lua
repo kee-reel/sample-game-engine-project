863539413eb3fff288c3edd64503783cfafd5b8b
kee-reel <c4rb0n_unit@protonmail.com>
2022-06-24 17:55:24 +0300

    More stars for multithreading test

--- assets/scripts/stars.lua
+++ assets/scripts/stars.lua
@@ -1,6 +1,5 @@
-
 stars = {}
-for i = 1, 1001, 1
+for i = 1, 10001, 1
 do
     star = app:add_game_object("materials/star.json", "scripts/star.lua", "")
     star.transform:set_pos(vec3:new(
@@ -14,9 +13,9 @@ end
 
 period = 0
 function update()
-    for i = 1, 1001, 1 do
-        star = stars[i]
-        star.transform:move(vec3:new(0, math.sin(period)/50, math.cos(period)/50));
-    end
-    period = period + 0.005
+    --for i = 1, 10001, 1 do
+    --    star = stars[i]
+    --    star.transform:move(vec3:new(0, math.sin(period)/50, math.cos(period)/50));
+    --end
+    --period = period + 0.005
 end
